05344a800e0493432ed05e6b2c0529ce1405f199
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-11-22 20:52:23 +0200

    overflow

--- src/helpers/Modal.jsx
+++ src/helpers/Modal.jsx
@@ -31,7 +31,7 @@ export default class Modal extends Component {
         className="fixed top-0 left-0 w-full h-full bg-slate-900 bg-opacity-80 flex justify-center items-center z-30"
         onClick={this.handleBackdropClick}
       >
-        <div className=" bg-opacity-70 rounded max-w-[90%] md:max-w-[720px] p-2 overflow-scroll">
+        <div className=" bg-opacity-70 rounded max-w-[90%] md:max-w-[720px] p-2 overflow-auto">
           {this.props.children}
         </div>
       </div>,
